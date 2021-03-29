a0611dad4ea57202cdaf980470d2f3bb05fcd086
Behemoth <simonostini@gmail.com>
2021-03-29 16:06:53 +0200

    🚀 update

--- src/authorization/admin.ts
+++ src/authorization/admin.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import * as jwt from "jsonwebtoken";
+import * as bcrypt from "bcrypt";
 
 const authenticateAdmin = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers["authorization"];
