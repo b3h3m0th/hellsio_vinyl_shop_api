0a74c58c74152f0f8e79864f3d8321409cbba3d7
Behemoth <simonostini@gmail.com>
2021-03-29 16:10:00 +0200

    ✨ update

--- src/authorization/admin.ts
+++ src/authorization/admin.ts
@@ -3,6 +3,7 @@ import * as jwt from "jsonwebtoken";
 import * as bcrypt from "bcrypt";
 
 const authenticateAdmin = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.body.pw) return res.sendStatus(403);
   const authHeader = req.headers["authorization"];
 };
 
