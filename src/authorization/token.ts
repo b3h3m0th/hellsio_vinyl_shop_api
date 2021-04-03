75ce1b4333b302648dd1513298c7440470c8325c
Behemoth <simonostini@gmail.com>
2021-04-03 14:40:20 +0200

    💄 update

--- src/authorization/token.ts
+++ src/authorization/token.ts
@@ -1,7 +1,7 @@
 import * as jwt from "jsonwebtoken";
 
 const generateAccessToken: (user: any) => string = (user: any) => {
-  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10s" });
 };
 
 export default generateAccessToken;
