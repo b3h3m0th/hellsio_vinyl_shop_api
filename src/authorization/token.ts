8985da21fe0e012589aa16caa38bfecf8e84fb8b
Behemoth <simonostini@gmail.com>
2021-04-05 11:43:42 +0200

    🎨 changed jwt expiring

--- src/authorization/token.ts
+++ src/authorization/token.ts
@@ -2,7 +2,7 @@ import * as jwt from "jsonwebtoken";
 
 const generateAccessToken: (user: any) => string = (user: any) => {
   return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "15min",
+    expiresIn: "10s",
   });
 };
 
