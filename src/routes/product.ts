8001e8a0f1a864713878853d4ee72540303d76b9
Behemoth <simonostini@gmail.com>
2021-04-13 12:45:29 +0200

    🎨 mayhem

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -13,6 +13,19 @@ router.get("/", (req: Request, res: Response) => {
   });
 });
 
+router.get("/:code", (req: Request, res: Response) => {
+  db.query(
+    `SELECT * from album WHERE code = ?`,
+    [req.params.code],
+    (err: MysqlError, results, fields) => {
+      if (err) return res.status(500).json({ error: "server error" });
+      if (results.length === 0) res.status(404).json({ error: "empty" });
+      console.log(results[0]);
+      return res.send(results[0]);
+    }
+  );
+});
+
 router.post("/add", authenticateAdminToken, (req: Request, res: Response) => {
   res.json({ add: "add" });
 });
