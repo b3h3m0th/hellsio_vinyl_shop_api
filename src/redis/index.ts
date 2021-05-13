93b18b8138f28259f685b87fd563075e2326f42e
Behemoth <simonostini@gmail.com>
2021-05-13 11:26:11 +0200

    ✅ update

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -4,7 +4,14 @@ export const redisHellsioPrefix = "hellsio-" as const;
 
 const client = redis.createClient({
   port: process.env.REDISPORT || 6379,
-  password: process.env.REDIS_AUTH || "",
 } as redis.ClientOpts);
+
+client.on("error", (err: redis.RedisError) => {
+  console.log(err);
+});
+
+client.on("message", (channel, message) => {
+  console.log(channel, message);
+});
 
 export default client;
