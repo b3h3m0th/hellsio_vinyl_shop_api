f0ce141064e5cc1d99fadb092eb57764afa1f35a
Behemoth <simonostini@gmail.com>
2021-05-16 18:22:35 +0200

    🚑 update

--- src/routes/rating.ts
+++ src/routes/rating.ts
@@ -1,14 +1,34 @@
 import * as express from "express";
 import { Request, Response } from "express";
+import { MysqlError } from "mysql";
 const router = express.Router();
-import authenticateUserToken, { isEmailVerified } from "../authorization/user";
+import authenticateUserToken from "../authorization/user";
 import db from "../database";
 
 router.post(
   "/rate",
   authenticateUserToken,
-  async (req: Request, res: Response) => {
-    return res.json({ endpoint: "rating" });
+  async (req: Request & { user: any }, res: Response) => {
+    console.log(req.body);
+    if (!req.user.email.includes("@")) {
+      db.query(
+        `REPLACE INTO rating (rating_id, value, Album_album_id, User_user_id) VALUES (NULL, ?, (SELECT album.album_id FROM album WHERE album.code = ?), (SELECT user.user_id FROM user WHERE user.username = ?));`,
+        [+req.body.value, req.body.album_code, req.user.email],
+        (err: MysqlError) => {
+          if (err) return res.sendStatus(500);
+          return res.sendStatus(201);
+        }
+      );
+    } else {
+      db.query(
+        `REPLACE INTO rating (rating_id, value, Album_album_id, User_user_id) VALUES (NULL, ?, (SELECT album.album_id FROM album WHERE album.code = ?), (SELECT user.user_id FROM user WHERE user.email = ?));`,
+        [+req.body.value, req.body.album_code, req.user.email],
+        (err: MysqlError) => {
+          if (err) return res.sendStatus(500);
+          return res.sendStatus(201);
+        }
+      );
+    }
   }
 );
 
